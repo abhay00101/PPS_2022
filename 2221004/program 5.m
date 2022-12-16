c2ac9ec53257a2ff59de33caa56c31ed201a4db3
Abhi-2221004 <116051389+Abhi-2221004@users.noreply.github.com>
2022-12-16 20:36:33 +0530

    Program05.md

--- 2221004/program 5.m
+++ 2221004/program 5.m
@@ -1,4 +1,4 @@
-// print number using do while
+## print number using do while
 #include<stdio.h>
 int main()
 {
@@ -11,3 +11,14 @@ a=a + 1;
 while(a<20);
 return 0;
 }
+## Output:-
+Value of a: 10
+Value of a: 11
+Value of a: 12
+Value of a: 13
+Value of a: 14
+Value of a: 15
+Value of a: 16
+Value of a: 17 
+Value of a: 18
+Value of a: 19
